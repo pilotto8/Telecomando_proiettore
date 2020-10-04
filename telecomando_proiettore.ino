4b78f77b9fbd7bda19ba422964bd45f4183ee2c3
pilotto8 <66770848+pilotto8@users.noreply.github.com>
2020-10-04 20:38:19 +0200

    Aggiunto segnale manuale (non funzia)

--- telecomando_proiettore.ino
+++ telecomando_proiettore.ino
@@ -1,6 +1,6 @@
 #include <attiny85_ir_send.h>
 IRsend irsend;
-enum tipologie{
+enum tipologie {
   NEC,
   SONY,
   RC5,
@@ -15,34 +15,37 @@ void setup() {
 }
 
 void loop() {
+
   if (digitalRead(A1) == LOW) {
-    mandaSegnale(codec, 0x212FD02F, 32);
+    mandaSegnale(0x212FD02F, 8);
   }
   else if (digitalRead(A2) == LOW) {
-    mandaSegnale(codec, 0x212F34CB, 32);
+    mandaSegnale(0x212F34CB, 8);
   }
   else if (digitalRead(A3) == LOW) {
-    mandaSegnale(codec, 0x212F34CB, 32);
+    mandaSegnale(0x212F34CB, 8);
   }
 }
 void mandaSegnale(int tipo, unsigned long int valore, int bits) {
-  switch (tipo){
-    case NEC:irsend.sendNEC(valore, bits);break;
-    case SONY:irsend.sendSony(valore, bits);break;
-    case RC5:irsend.sendRC5(valore, bits);break;
-    case RC6:irsend.sendRC6(valore, bits);break;
+  switch (tipo) {
+    case NEC: irsend.sendNEC(valore, bits); break;
+    case SONY: irsend.sendSony(valore, bits); break;
+    case RC5: irsend.sendRC5(valore, bits); break;
+    case RC6: irsend.sendRC6(valore, bits); break;
   }
   delay(500);
 }
-void segnaleManuale(unsigned long int valore, int bits, int frequenza){
-  for(int i = bits; i > 0; i--){
-    if (valore % 2 == 1){
-      digitalWrite(PB1, HIGH);
+void mandaSegnale(unsigned long int valore, int frequenza) {
+  while (valore > 0) {
+    if (valore % 2 == 1) {
+      analogWrite(PB1, 255);
     }
-    else{
-      digitalWrite(PB1, LOW);
+    else {
+      analogWrite(PB1, 0);
     }
     valore /= 2;
-    delay(1);
+    delayMicroseconds(1000 / frequenza);
   }
+  digitalWrite(PB1, LOW);
+  delay(500);
 }
